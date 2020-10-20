b91dd4de5713a7423fd098a5e1dcd7228283ab51
Samiya Akhtar <samiyaakhtar7@gmail.com>
2020-10-20 15:58:38 -0700

    Updates

--- _next/static/UQZhXYlcMmS7J01qmX7yH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002Fe02aaf40.803f6da08ecd7afd1728.js","static\u002Fcss\u002Fe3b561f95a3f5801305c.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
